df96a42fd880485bba4ce06d746b829a9d9611de
KenetAndres-gitHub <kenbok2014@hotmail.com>
2024-02-12 12:10:43 -0500

    Updated

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/person')]
+#[Route('/')]
 class PersonController extends AbstractController
 {
     #[Route('/', name: 'app_person_index', methods: ['GET'])]
